f169ea84bd4c2937f3533155c5d9b614cc9a84c8
akanaskhan <muhammadanaskhanak@gmail.com>
2024-05-09 10:22:19 +0500

    updated

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var currentDate=new Date();
 var eidDate= new Date("16 june 2024");
 var diff=(eidDate -currentDate)/1000;
 
-var days=Math.floor(diff/3600/24);
+// var months=Math.floor((diff/3600) % 24);
+var months=Math.floor((diff/3600/24)%12);
+var Rdays=Math.floor(diff/3600/24);
+var days=Math.floor((diff/3600/24)-30);
 var hours=Math.floor((diff/3600) % 24);
 var minuts=Math.floor((diff/60)%60);
 var seconds=Math.floor(diff%60);
 
 if (diff<0) return;
+document.getElementById("months").innerHTML= months;
 document.getElementById("main").innerHTML= days;
 document.getElementById("main_hours").innerHTML= hours;
 document.getElementById("main_minuts").innerHTML= minuts;
@@ -23,3 +27,4 @@ setInterval(
     },
     1000
     )
+console.log(months)
